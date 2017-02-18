81e24c1a6a52646b8850da87ac47a8c750f3aaf1
Davi Oliveira <davijff@gmail.com>
2017-02-18 17:49:44 -0200

    Projeto iniciado

---
+++ desktop/src/com/tutdria/gravity/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tutdria.gravity.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tutdria.gravity.GravityGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GravityGame(), config);
+	}
+}
